57bae62b44bca93902beb817a59aefebd3c783df
Josh Neta <joshneta@gmail.com>
2017-07-29 13:32:00 -0500

    Add "String to Integer (atoi)"

---
+++ Problems/String to Integer (atoi)/MyAtoi.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.StringToInteger
+{
+    public static class Solution
+    {
+        public static int MyAtoi(string str)
+        {
+            var s = str.Trim();
+            if(s.Length == 0)
+            {
+                return 0;
+            }
+
+            long ret = 0;
+            int sign = 1;
+
+            if(s[0] == '-')
+            {
+                sign = -1;
+            }
+
+            int start = 0;
+            if(s[0] == '-' || s[0] == '+')
+            {
+                start = 1;
+            }
+
+            try
+            {
+                for (int i = start; i < s.Length; i++)
+                {
+                    ret = ret * 10 + CharToInt(s[i]);
+                }
+            }
+            catch
+            {
+                return (int)ret * sign;
+            }
+
+            if(ret > Int32.MaxValue)
+            {
+                return 0;
+            }
+            if(ret < Int32.MinValue)
+            {
+                return 0;
+            }
+
+            return (int)ret * sign;
+        }
+
+        public static int CharToInt(char c)
+        {
+            switch(c)
+            {
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                case '0':
+                    return c - 48;
+            }
+
+            throw new InvalidOperationException();
+        }
+    }
+}
---
+++ Tests/String to Integer (atoi)/MyAtoi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Tests.MyAtoi
+{
+    public class MyAtoi
+    {
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("10", 10)]
+        [InlineData("1000000000000", 0)]
+        [InlineData("-1", -1)]
+        [InlineData("-1000000000000", 0)]
+        [InlineData("12340", 12340)]
+        [InlineData("01", 1)]
+        [InlineData("10 asfd", 10)]
+        [InlineData("+ 10 asfd", 0)]
+        [InlineData("- 10 asfd", 0)]
+        [InlineData("+ 10", 0)]
+        [InlineData("+1", 1)]
+        [InlineData("+10", 10)]
+        [InlineData("+1000000000000", 0)]
+        [InlineData("", 0)]
+        public void TestMyAtoi(string input, int expected)
+        {
+            var actual = Problems.StringToInteger.Solution.MyAtoi(input);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
