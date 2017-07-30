0a010cc8b01b0e55c5670d9ce53f6a653875fb5a
Josh Neta <joshneta@gmail.com>
2017-07-29 20:58:35 -0500

    Add "Regular Expression Matching"

---
+++ Problems/Regular Expression Matching/RegularExpressionMatching.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.RegularExpressionMatching
+{
+    public static class RegularExpressionMatching
+    {
+        public static bool IsMatch(string s, string p)
+        {
+            if(string.IsNullOrEmpty(p))
+            {
+                return string.IsNullOrEmpty(s);
+            }
+
+            if(p.Length > 1 && '*' == p[1])
+            {
+                // x*
+                return (IsMatch(s, p.Substring(2)) || (!string.IsNullOrEmpty(s) && (s[0] == p[0] || '.' == p[0]) && IsMatch(s.Substring(1), p)));
+            }
+            else
+            {
+                return !string.IsNullOrEmpty(s) && (s[0] == p[0] || '.' == p[0]) && IsMatch(s.Substring(1), p.Substring(1));
+            }
+        }
+    }
+}
---
+++ Tests/Regular Expression Matching/RegularExpressionMatching.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Tests.Regular_Expression_Matching
+{
+    public class RegularExpressionMatching
+    {
+        [Theory]
+        [InlineData("aa", "a", false)]
+        [InlineData("aa", "aa", true)]
+        [InlineData("aaa", "aa", false)]
+        [InlineData("aa", "a*", true)]
+        [InlineData("aa", ".*", true)]
+        [InlineData("ab", ".*", true)]
+        [InlineData("aab", "c*a*b", true)]
+        [InlineData("", "", true)]
+        [InlineData("a", ".", true)]
+        [InlineData("abb", "ab.", true)]
+        [InlineData("abb", "ab.*", true)]
+        [InlineData("abbbc", "ab.*", true)]
+        [InlineData("abcd", "d*", false)]
+        [InlineData("abbc", "ab.", false)]
+        [InlineData("ab", ".*c", false)]
+        [InlineData("abbaa", "ab.*a", true)]
+        [InlineData("abbaac", "ab.*a", false)]
+        [InlineData("abbaac", "ab.*a.*", true)]
+        [InlineData("abbaac", "ab.*ac", true)]
+        [InlineData("abbaacc", "ab.*ac", false)]
+        public void TestIsMatch(string s, string p, bool expected)
+        {
+            var actual = Problems.RegularExpressionMatching.RegularExpressionMatching.IsMatch(s, p);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
