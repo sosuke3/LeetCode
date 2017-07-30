8e1889ac3cf3f9d8d68ab29417693883473c88d7
Josh Neta <joshneta@gmail.com>
2017-07-30 10:16:11 -0500

    Add "Palindrome Number"

---
+++ Problems/Palindrome Number/PalindromeNumber.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.PalindromeNumber
+{
+    public static class PalindromeNumber
+    {
+        public static bool IsPalindrome(int x, int expectedCount = 0)
+        {
+            if(x < 0)
+            {
+                return false;
+            }
+            if (x == 0)
+            {
+                return true;
+            }
+
+            int rightDigit = x % 10;
+            int leftDigit = x;
+            int count = 0;
+            while(leftDigit > 9)
+            {
+                leftDigit = leftDigit / 10;
+                count++;
+            }
+            if(expectedCount > count)
+            {
+                count = expectedCount;
+                leftDigit = 0;
+            }
+
+            if(rightDigit == leftDigit)
+            {
+                if(count == 0)
+                {
+                    return true;
+                }
+
+                int left = (leftDigit * (int)Math.Pow(10, count));
+
+                int next = (x - left - rightDigit) / 10;
+
+                return IsPalindrome(next, count - 2);
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Tests/Palindrome Number/PalindromeNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Tests.Palindrome_Number
+{
+    public class PalindromeNumber
+    {
+        [Theory]
+        [InlineData(0, true)]
+        [InlineData(-1, false)]
+        [InlineData(123, false)]
+        [InlineData(2332, true)]
+        [InlineData(2342, false)]
+        [InlineData(Int32.MaxValue, false)]
+        [InlineData(313, true)]
+        [InlineData(1001, true)]
+        [InlineData(100021, false)]
+        [InlineData(1000110001, true)]
+        public void TestIsPalindrome(int x, bool expected)
+        {
+            var actual = Problems.PalindromeNumber.PalindromeNumber.IsPalindrome(x);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
