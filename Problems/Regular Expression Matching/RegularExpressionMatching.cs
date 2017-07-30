4ba98fc1b94f27b402e8158b23aa36480dd86e4a
Josh Neta <joshneta@gmail.com>
2017-07-30 09:17:37 -0500

    Redid regex

--- Problems/Regular Expression Matching/RegularExpressionMatching.cs
+++ Problems/Regular Expression Matching/RegularExpressionMatching.cs
@@ -10,20 +10,41 @@ namespace Problems.RegularExpressionMatching
     {
         public static bool IsMatch(string s, string p)
         {
-            if(string.IsNullOrEmpty(p))
+            if (string.IsNullOrEmpty(p))
             {
                 return string.IsNullOrEmpty(s);
             }
 
-            if(p.Length > 1 && '*' == p[1])
+            if (s == p)
             {
-                // x*
-                return (IsMatch(s, p.Substring(2)) || (!string.IsNullOrEmpty(s) && (s[0] == p[0] || '.' == p[0]) && IsMatch(s.Substring(1), p)));
+                return true;
             }
+
+            // x*
+            if (p.Length > 1 && '*' == p[1])
+            {
+                // x* matches x, advance to next s and check it
+                if (!string.IsNullOrEmpty(s) && (s[0] == p[0] || '.' == p[0]) && IsMatch(s.Substring(1), p))
+                {
+                    return true;
+                }
+                // x* doesn't match x, skip to next p
+                if (IsMatch(s, p.Substring(2)))
+                {
+                    return true;
+                }
+            }
+            // xy
             else
             {
-                return !string.IsNullOrEmpty(s) && (s[0] == p[0] || '.' == p[0]) && IsMatch(s.Substring(1), p.Substring(1));
+                // x matchs x, advance both to the next substring
+                if (!string.IsNullOrEmpty(s) && (s[0] == p[0] || '.' == p[0]) && IsMatch(s.Substring(1), p.Substring(1)))
+                {
+                    return true;
+                }
             }
+
+            return false;
         }
     }
 }
