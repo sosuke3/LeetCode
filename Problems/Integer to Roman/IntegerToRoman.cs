e34244f35f7e9b5e1e59ee96b79d9952147afc43
Josh Neta <joshneta.github@gmail.com>
2017-08-09 09:51:04 -0500

    Add "Integer to Roman"

---
+++ Problems/Integer to Roman/IntegerToRoman.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.IntegerToRoman
+{
+    public static class Solution
+    {
+        public static string IntToRoman(int num)
+        {
+            if(num <= 0)
+            {
+                return "";
+            }
+
+            var sb = new StringBuilder();
+
+            int thousandDigit = (num / 1000) % 10;
+            int hundredDigit = (num / 100) % 10;
+            int tenDigit = (num / 10) % 10;
+            int firstDigit = num % 10;
+
+            BuildDigit(thousandDigit, sb, "M", "V̅", "X̅");
+            BuildDigit(hundredDigit, sb, "C", "D", "M");
+            BuildDigit(tenDigit, sb, "X", "L", "C");
+            BuildDigit(firstDigit, sb, "I", "V", "X");
+
+            return sb.ToString();
+        }
+
+        public static void BuildDigit(int digit, StringBuilder sb, string One, string Five, string Ten)
+        {
+            if(digit >= 5 && digit < 9)
+            {
+                sb.Append(Five);
+            }
+            if(digit == 4 || digit == 9)
+            {
+                sb.Append(One);
+            }
+            if(digit == 1 || digit == 6)
+            {
+                sb.Append(One);
+            }
+            if(digit == 2 || digit == 7)
+            {
+                sb.Append(One[0], 2);
+            }
+            if (digit == 3 || digit == 8)
+            {
+                sb.Append(One[0], 3);
+            }
+            if(digit == 4)
+            {
+                sb.Append(Five);
+            }
+            if(digit == 9)
+            {
+                sb.Append(Ten);
+            }
+        }
+    }
+}
---
+++ Tests/Integer to Roman/IntegerToRoman.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+using Problems.IntegerToRoman;
+
+namespace Tests.IntegerToRoman
+{
+    public class IntegerToRoman
+    {
+        [Theory]
+        [InlineData(1, "I")]
+        [InlineData(2, "II")]
+        [InlineData(3, "III")]
+        [InlineData(4, "IV")]
+        [InlineData(5, "V")]
+        [InlineData(6, "VI")]
+        [InlineData(7, "VII")]
+        [InlineData(8, "VIII")]
+        [InlineData(9, "IX")]
+        [InlineData(10, "X")]
+        [InlineData(11, "XI")]
+        [InlineData(12, "XII")]
+        [InlineData(13, "XIII")]
+        [InlineData(14, "XIV")]
+        [InlineData(15, "XV")]
+        [InlineData(16, "XVI")]
+        [InlineData(40, "XL")]
+        [InlineData(70, "LXX")]
+        [InlineData(90, "XC")]
+        [InlineData(400, "CD")]
+        [InlineData(900, "CM")]
+        [InlineData(1200, "MCC")]
+        public void TestIntToRoman(int num, string expected)
+        {
+            var actual = Solution.IntToRoman(num);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
